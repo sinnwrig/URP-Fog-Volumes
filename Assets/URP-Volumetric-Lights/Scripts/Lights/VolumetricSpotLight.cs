38b9d03c825fab8f9f05f954f3017b6186d6f1dc
sinnwrig <ExplodingKittyBombOfDeath@gmail.com>
2023-09-20 18:58:39 -0700

    ihjg

--- Assets/URP-Volumetric-Lights/Scripts/Lights/VolumetricSpotLight.cs
+++ Assets/URP-Volumetric-Lights/Scripts/Lights/VolumetricSpotLight.cs
@@ -8,25 +8,4 @@ public partial class VolumetricLight
     {
         
     }
-
-
-    private bool IsCameraInSpotLightBounds()
-    {
-        // check range
-        float distance = Vector3.Dot(_light.transform.forward, Camera.current.transform.position - _light.transform.position);
-        float extendedRange = _light.range + 1;
-        if (distance > extendedRange)
-        {
-            return false;
-        }
-
-        // check angle
-        float cosAngle = Vector3.Dot(transform.forward, (Camera.current.transform.position - _light.transform.position).normalized);
-        if((Mathf.Acos(cosAngle) * Mathf.Rad2Deg) > (_light.spotAngle + 3) * 0.5f)
-        {
-            return false;
-        }
-
-        return true;
-    }
 }

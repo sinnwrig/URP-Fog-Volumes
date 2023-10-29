1d78d1f9019c2770b1e11ffd228d41ada6ec537a
sinnwrig <ExplodingKittyBombOfDeath@gmail.com>
2023-10-29 01:24:44 -0700

    lights show up now

--- Assets/URP-Volumetric-Lights/Scripts/Lights/VolumetricDirectionalLight.cs
+++ Assets/URP-Volumetric-Lights/Scripts/Lights/VolumetricDirectionalLight.cs
@@ -4,8 +4,9 @@ using UnityEngine.Rendering;
 
 public partial class VolumetricLight
 {
-    private void SetupDirectionalLight(VolumetricLightPass pass)
+    private void SetupDirectionalLight(Material volumetricMaterial, VolumetricLightPass pass)
     {
-        
+        // Pass 2 - Directional
+        BlitUtility.BlitNext(volumetricMaterial, "_SourceTexture", 2);
     }
 }
--- Assets/URP-Volumetric-Lights/Scripts/VolumetricLightPass.cs
+++ Assets/URP-Volumetric-Lights/Scripts/VolumetricLightPass.cs
@@ -1,47 +1,41 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering.Universal.Internal;
 using System.Collections.Generic;
 using System;
+using Unity.Collections;
+using System.Reflection;
 
 public partial class VolumetricLightPass : ScriptableRenderPass
 {
+    private static readonly FieldInfo shadowCasterField = typeof(UniversalRenderer).GetField("m_AdditionalLightsShadowCasterPass", BindingFlags.NonPublic | BindingFlags.Instance);
+    private static readonly Plane[] frustumPlanes = new Plane[6];
+
+
+    public struct SortedLight
+    {
+        public VisibleLight visibleLight;
+        public int lightIndex;
+        public int shadowIndex;
+        public Light light => visibleLight.light;
+    }
+
+
     public VolumetricResolution resolution;
+    public VolumetricLightFeature feature;
+
+    public float lightRange, falloffRange;
+
 
     private static Material bilateralBlur;
-    private static Shader volumetricLight;
-
-    private static Material volumeLightMat;
+    private static Material volumetricLight;
 
 
     private static Texture3D noiseTexture;
     private static Texture2D ditherTexture;
 
     private CommandBuffer commandBuffer;
-
-    public VolumetricLightFeature feature;
-
-
-    private static readonly List<VolumetricLight> activeLights = new();
-
-
-    public static void AddVolumetricLight(VolumetricLight light)
-    {
-        if (!activeLights.Contains(light))
-        {
-            activeLights.Add(light);
-
-            if (volumetricLight != null) 
-            {
-                light.Setup(volumetricLight);
-            }
-        }
-    }
-
-    public static void RemoveVolumetricLight(VolumetricLight light)
-    {
-        activeLights.Remove(light);
-    }
     
 
 
@@ -50,15 +44,8 @@ public partial class VolumetricLightPass : ScriptableRenderPass
         if (bilateralBlur == null || bilateralBlur.shader != blur)
             bilateralBlur = new Material(blur);
 
-        volumetricLight = light;
-
-        foreach (VolumetricLight activeLight in activeLights)
-        {
-            activeLight.Setup(volumetricLight);
-        }
-
-        if (volumeLightMat == null || volumeLightMat.shader != light)
-            volumeLightMat = new Material(light);
+        if (volumetricLight == null || volumetricLight.shader != light)
+            volumetricLight = new Material(light);
 
         ValidateResources();
     }   
@@ -74,17 +61,110 @@ public partial class VolumetricLightPass : ScriptableRenderPass
     }
 
 
+
+    private bool LightIsVisible(ref VisibleLight visibleLight, Plane[] frustumPlanes, Camera camera) 
+	{
+        if (visibleLight.lightType == LightType.Directional)
+            return true;
+
+        Light light = visibleLight.light;
+        Vector3 position = light.transform.position;
+
+		// Cull spherical range, ignoring camera far plane at index 5
+		for (int i = 0; i < frustumPlanes.Length; i++) 
+		{
+			float distance = frustumPlanes[i].GetDistanceToPoint(position);
+
+			if (distance < 0 && Mathf.Abs(distance) > light.range) 
+				return false;
+		}
+
+        float distanceToSphereBounds = (camera.transform.position - position).magnitude - light.range;
+
+        // Cull faraway lights
+        if (distanceToSphereBounds > lightRange)
+            return false;
+
+		return true;
+	}
+
+
+    private List<SortedLight> GetSortedLights(ref RenderingData renderingData)
+    {
+        AdditionalLightsShadowCasterPass shadowCasterPass = (AdditionalLightsShadowCasterPass)shadowCasterField.GetValue(renderingData.cameraData.renderer);
+
+        ref LightData lightData = ref renderingData.lightData;
+        ref NativeArray<VisibleLight> visibleLights = ref lightData.visibleLights;
+
+        List<SortedLight> sortedLights = new();
+
+
+        GeometryUtility.CalculateFrustumPlanes(renderingData.cameraData.camera, frustumPlanes);
+
+        
+        for (int i = 0; i < visibleLights.Length; i++)
+        {
+            var visibleLight = visibleLights[i];
+
+            if (LightIsVisible(ref visibleLight, frustumPlanes, renderingData.cameraData.camera))
+            {   
+                sortedLights.Add(new SortedLight
+                {
+                    visibleLight = visibleLight,
+                    lightIndex = i,
+                    shadowIndex = shadowCasterPass.GetShadowLightIndexFromLightIndex(i)
+                });
+            }
+        }
+
+        return sortedLights;
+    }
+
+
+
     public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
     {
         var source = renderingData.cameraData.renderer.cameraColorTargetHandle;
-        var descriptor = renderingData.cameraData.cameraTargetDescriptor;
+        ref var lightData = ref renderingData.lightData;
 
         commandBuffer = CommandBufferPool.Get("Volumetric Light Pass");
 
         DownsampleDepthBuffer();
 
+        BlitUtility.BeginBlitLoop(commandBuffer, source);
+
+        var lights = GetSortedLights(ref renderingData);
+
+        CheckLogging();
+
+        Log($"Light count: {lightData.additionalLightsCount}");
+        Log($"Max lights: {lightData.maxPerObjectAdditionalLightsCount}");
+
+
+        commandBuffer.SetGlobalVector("_LightRange", new Vector2(lightRange, falloffRange));
+
+        for (int i = 0; i < lights.Count; i++)
+        {
+            string isMain = "";
+            if (lights[i].lightIndex == lightData.mainLightIndex)
+            {
+                isMain = "main ";
+            }
+
+            // Would it be better to cache this GetComponent call? Yes.
+            // Is it minor compared to the actual rendering of the light? Yes. 
+            if (lights[i].light.TryGetComponent(out VolumetricLight lightComponent))
+            {
+                Log($"Drawing {isMain}light. Index {i}, Name {lightComponent.gameObject.name}");
+                lightComponent.DrawLight(volumetricLight, this);
+            }
+        }
+
+
+        BlitUtility.EndBlitLoop(source);
+
         // Blit for every active volumetric light
-        //commandBuffer.Blit(source, volumeLightTexture, volumeLightMat);  
+        //commandBuffer.Blit(source, volumeLightTexture, volumetricLight);  
 
 
         // Blur and upsample volumetric light texture
@@ -101,11 +181,26 @@ public partial class VolumetricLightPass : ScriptableRenderPass
     }
 
 
-    private static Vector3 ClampMin(Vector3 vector)
+
+
+    float lastTime = 0.0f;
+    bool canLog = false;
+
+    void CheckLogging()
+    {   
+        canLog = false;
+
+        if (Mathf.Abs(lastTime - Time.time) > 5.0f)
+        {
+            lastTime = Time.time;
+            canLog = true;
+        }
+    }
+
+
+    void Log(string message)
     {
-        vector.x = vector.x == 0.0 ? 1e-20f : vector.x;
-        vector.y = vector.y == 0.0 ? 1e-20f : vector.y;
-        vector.z = vector.z == 0.0 ? 1e-20f : vector.z;
-        return vector;
+        if (canLog)
+            Debug.Log(message);
     }
 }
